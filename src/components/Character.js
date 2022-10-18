bb4b32fa9764385bf148f1c7b0c534d9271b683f
Fatimah Ismael <fatimahismael@yahoo.com>
2022-10-18 10:22:53 -0700

    passed tests

--- src/components/Character.js
+++ src/components/Character.js
@@ -3,37 +3,52 @@ import React, { useState, useEffect }from "react"
 import { data } from "../mocks/handlers"
 import axios from 'axios';
 
- function Character(props){
-    const [starWars, setStarWars] = useState(data);
+ function Character({ props }){
+    const [starWars, setStarWars] = useState([]);
+
+   /* useEffect(()=>{
+    const url = 'https://swapi.dev/api/people/';
+
+   const fetchData = async() => {
+    try{
+        const response = await fetch(url);
+        const json =await response.json();
+        console.log(data);
+        setStarWars(data)
+    } catch(error) {
+        console.log('error', error);
+    }
+   };
+   fetchData();
+}, []);
+
+    */
+    useEffect(() => {
+        axios.get(`https://swapi.dev/api/people/`)
+        .then(res => {
+          //const element= document.querySelector()
+          console.log(res.data);
+         setStarWars(res.data);
+        }).catch(err => console.error(err))
+      }, [])
     
+    return ( 
+        <div className= 'character container'>
+            {starWars.map(char =>
+                <div className="characters" key={char.name}>
+                    {char.name}  Birth Year: {char.birth_year}
+                </div>
+                    )}
     
+        </div>
+           
+          )
+            
 
-/*
-useEffect(() => {
-  axios.get(`https://swapi.dev/api/people/`)
-  .then(res => {
-   setData(res.data);
-  }).catch(err => console.error(err))
-}, [])
 
-/*--------
-insert under return 
-<h2>Name: {starWars}</h2>
-*/
 
-return ( 
-    <div className= 'character container'>
-        {data.map(char =>
-            <div className="characters" key={char.name}>
-                {char.name}  Birth Year: {char.birth_year}
-            </div>
-                )}
-
-    </div>
-       
-      )
-}
-
+        }
+        
     
   export default Character;
 
